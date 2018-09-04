920863d1cfc72b47d779c8d4dfab91f7f54ac540
Sweetmimike <aurecarp@live.fr>
2018-09-04 22:15:46 +0200

    random

--- src/net/sweetmimike/automessage/Main.java
+++ src/net/sweetmimike/automessage/Main.java
@@ -1,21 +1,28 @@
 package net.sweetmimike.automessage;
 
+import java.util.List;
+import java.util.Random;
+
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class Main extends JavaPlugin {
 	
+	List<String> messages = getConfig().getStringList("message");
+	
 	@Override
 	public void onEnable() {
 		System.out.println("salut");
+		saveDefaultConfig();
 		super.onEnable();
 		
 		new BukkitRunnable() {
-			
+			Random rand = new Random();
 			@Override
 			public void run() {
-				Bukkit.broadcastMessage("salut");
+				int valeur = rand.nextInt(3);
+				Bukkit.broadcastMessage(messages.get(valeur));
 				
 			}
 		}.runTaskTimer(this, 0, 40);
