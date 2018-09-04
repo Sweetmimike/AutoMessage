932a43d9377d7813950409942c10c43209be01a8
Sweetmimike <aurecarp@live.fr>
2018-09-04 23:26:03 +0200

    changes

---
+++ src/net/sweetmimike/automessage/AutoMessager.java
@@ -0,0 +1,40 @@
+package net.sweetmimike.automessage;
+
+import java.util.List;
+import java.util.Random;
+
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class AutoMessager {
+	
+	Main main;
+	List<String> messages;
+	int nbMessage;
+	int valeur;
+	long cooldown;
+	
+	public AutoMessager(Main main) {
+		this.main = main;
+		messages = main.getConfig().getStringList("message");
+		nbMessage = messages.size();
+		cooldown = main.getConfig().getLong("cooldown") * 20;
+	}
+	
+	
+	
+	public void onRunMessage() {
+		
+		new BukkitRunnable() {
+			Random rand = new Random();
+			@Override
+			public void run() {
+				
+				valeur = rand.nextInt(nbMessage);
+				Bukkit.broadcastMessage(messages.get(valeur).replace("&", "§"));
+				
+			}
+		}.runTaskTimer(main, 0, cooldown);
+	}
+
+}
--- src/net/sweetmimike/automessage/Main.java
+++ src/net/sweetmimike/automessage/Main.java
@@ -1,39 +1,17 @@
 package net.sweetmimike.automessage;
 
-import java.util.List;
-import java.util.Random;
-
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
 public class Main extends JavaPlugin {
 	
-	List<String> messages = getConfig().getStringList("message");
-	int nbMessage = messages.size();
-	int valeur;
-	long cooldown = getConfig().getLong("cooldown") * 20;
+	private AutoMessager messager = new AutoMessager(this);
 	
 	@Override
 	public void onEnable() {
 		
+		messager.onRunMessage();
 		saveDefaultConfig();
 		super.onEnable();
-		
-		new BukkitRunnable() {
-			Random rand = new Random();
-			@Override
-			public void run() {
-				
-				valeur = rand.nextInt(nbMessage);
-				Bukkit.broadcastMessage(messages.get(valeur).replace("&", "§"));
-				System.out.println("salut");
-				System.out.println("salut");
-				System.out.println("salut");
-				System.out.println("salut");
-				
-			}
-		}.runTaskTimer(this, 0, cooldown);
 	}
 	
 	@Override
