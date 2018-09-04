526d62213839a872efcb5f0778c62c0ab130020f
Sweetmimike <aurecarp@live.fr>
2018-09-04 22:04:16 +0200

    add bukkitRunnable

--- src/net/sweetmimike/automessage/Main.java
+++ src/net/sweetmimike/automessage/Main.java
@@ -1,6 +1,8 @@
 package net.sweetmimike.automessage;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class Main extends JavaPlugin {
 	
@@ -8,5 +10,14 @@ public class Main extends JavaPlugin {
 	public void onEnable() {
 		System.out.println("salut");
 		super.onEnable();
+		
+		new BukkitRunnable() {
+			
+			@Override
+			public void run() {
+				Bukkit.broadcastMessage("salut");
+				
+			}
+		}.runTaskTimer(this, 0, 40);
 	}
 }
