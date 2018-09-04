9d7836e1e1e588290daa0487fb32600fba8e1f63
Sweetmimike <aurecarp@live.fr>
2018-09-04 21:47:50 +0200

    changes

--- src/net/sweetmimike/automessage/Main.java
+++ src/net/sweetmimike/automessage/Main.java
@@ -4,6 +4,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	
-	
-
+	@Override
+	public void onEnable() {
+		System.out.println("salut");
+		super.onEnable();
+	}
 }
