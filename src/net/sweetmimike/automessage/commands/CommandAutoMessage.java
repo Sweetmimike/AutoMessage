4d3aca0217ae3311e24d93253322ad7384565eb6
Sweetmimike <aurecarp@live.fr>
2018-09-05 19:35:52 +0200

    init automessage command

---
+++ src/net/sweetmimike/automessage/commands/CommandAutoMessage.java
@@ -0,0 +1,15 @@
+package net.sweetmimike.automessage.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandAutoMessage implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
